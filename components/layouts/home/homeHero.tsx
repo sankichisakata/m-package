3678f3839fa3dedff7332bc867b2295eab95a869
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2023-09-17 05:17:29 +0900

    drawer編集

--- components/layouts/home/homeHero.tsx
+++ components/layouts/home/homeHero.tsx
@@ -2,9 +2,9 @@ import HomeSwiper from "@/components/elements/swiper/homeSwiper";
 
 export default function HomeHero () {
     return(
-        <div className="h-screen w-screen  flex justify-center items-center">
+        <div className="h-screen w-screen flex justify-center items-center">
             <div className="
-            h-6/12 w-11/12          
+            h-6/12 w-11/12 z-[-10]
             border border-gray-100
             ">
                 <HomeSwiper/>
