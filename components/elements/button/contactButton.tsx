bb07c14d4d15184819d1325c9a43ac21191a3b5e
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2023-09-17 13:54:21 +0900

    drawer編集

--- components/elements/button/contactButton.tsx
+++ components/elements/button/contactButton.tsx
@@ -10,12 +10,12 @@ const ContactButton = () => {
         flex justify-center items-center
         '
         >
-            <Link href="/contact" className="border border-bg-slate-600 rounded-full">
+            <Link href="/contact" className="border border-slate-500/20 rounded-full">
               
                 <div className="flex justify-center items-center">
-                  <p className="px-2 py-2 text-gray-800 text-xxs lg:text-sm">お問い合わせ</p>
+                  <p className="px-2 py-2 text-slate-800 text-xxs lg:text-sm">お問い合わせ</p>
                   <EmailIcon
-                    className="border-gray-600 rounded-full p-1 m-1 bg-gray-600 text-white"
+                    className="border-slate-800 rounded-full p-1 m-1 bg-slate-800 text-white"
                     w={22} h={22}
                     />
                   </div>
--- components/elements/drawer/drawerContents.tsx
+++ components/elements/drawer/drawerContents.tsx
@@ -2,7 +2,7 @@
 
 import React, { useRef } from "react"
 import Link from "next/link"
-import { HamburgerIcon, EmailIcon, PhoneIcon, ChevronRightIcon } from "@chakra-ui/icons"
+import { HamburgerIcon, EmailIcon, PhoneIcon, ChevronRightIcon, SmallCloseIcon } from "@chakra-ui/icons"
 import {
     Drawer,
     DrawerBody,
@@ -15,32 +15,44 @@ import {
     Button,
     useDisclosure,
     Link as ChakraLink,
-    Divider,
+    Fade,
+    ScaleFade,
+    Slide,
+    SlideFade,
+    Collapse
   } from '@chakra-ui/react'
 
 export default function DrawerContents () {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose, onToggle } = useDisclosure()
     const btnRef = React.useRef<HTMLButtonElement>(null)
   
     return (
       <>
+        
         <Button className="
         block w-16
         p-3
-        " ref={btnRef} onClick={onOpen}>
-          <HamburgerIcon color='black' w={23} h={23}></HamburgerIcon>
+        " ref={btnRef} onClick={onToggle}>
+
+          { isOpen ? 
+            <SmallCloseIcon color='black' w={23} h={23}/>
+          :
+            <HamburgerIcon color='black' w={23} h={23}/>
+          }
+
         </Button>
 
         <Drawer
           isOpen={isOpen}
-          placement='top'
+          placement='bottom'
           onClose={onClose}
           finalFocusRef={btnRef}
         >
-          <DrawerOverlay className="bg-gray-300">
+          
+          <DrawerOverlay onClick={onClose} className="bg-slate-600/50">
             <DrawerContent className="w-full h-screen flex flex-col gap-5" overflow='scroll'>
                 
-                <DrawerHeader
+                {/* <DrawerHeader
                 className="
                 w-full pt-3
                 flex flex-col items-center
@@ -48,32 +60,33 @@ export default function DrawerContents () {
                 >
                   <div className="
                   w-11/12 
-                  bg-slate-600 rounded-md shadow-2xl
+                  bg-white rounded-md shadow-2xl
                   flex justify-between items-center
                   ">
                    <div className='w-52'>
                     <Link onClick={onClose} href="/">
-                          <p className='p-3 text-xxs text-white text-bold'>株式会社エムパッケージ</p>
+                          <p className='p-3 text-xxs text-slate-600 text-bold'>株式会社エムパッケージ</p>
                     </Link>
                   </div>
                   <DrawerCloseButton className="
                   w-16
                   inline-block p-3
-                  text-lg font-bold text-slate-100
+                  text-lg font-bold text-slate-600
                   "
                   />
                   </div>
-                </DrawerHeader>
+                </DrawerHeader> */}
 
     
                 <DrawerBody className="
+                mt-20
                 w-11/12 mx-auto overflow-scroll 
                 shadow-2xl rounded-md
                 "
                 >
                   <div className='
                   w-full
-                  bg-slate-600 mx-auto px-5 py-8
+                  bg-white mx-auto px-5 py-8
                   '
                   >
                       <nav className='
@@ -83,24 +96,24 @@ export default function DrawerContents () {
                       >
 
                             <div className='w-full md:w-5/6 flex justify-center items-center gap-2 md:gap-6'>
-                              <Link href="/contact" onClick={onClose} className="border border-gray-100/20 rounded-full">
+                              <Link href="/contact" onClick={onClose} className="border border-slate-500/20 rounded-full">
                                 <ChakraLink>
                                   <div className="flex justify-center items-center">
-                                    <p className="px-3 py-2 text-orange-400 text-xs">お問い合わせ</p>
+                                    <p className="px-3 py-2 text-slate-800 text-xs">お問い合わせ</p>
                                     <EmailIcon
-                                      className="border-gray-100 rounded-full p-1 m-1 bg-gray-100 text-slate-600"
+                                      className="border-slate-800 rounded-full p-1 m-1 bg-slate-800 text-white"
                                       w={26} h={26}
                                       />
-                                    </div>
+                                  </div>
                                 </ChakraLink>
                               </Link>
 
-                                <Link href="/contact" onClick={onClose} className="border border-gray-100/20 rounded-full">
+                                <Link href="/contact" onClick={onClose} className="border border-slate-500/20 rounded-full">
                                   <ChakraLink>
                                     <div className="flex justify-center items-center">
-                                      <p className="px-3 py-2 text-orange-400 text-xs">電話で相談する</p>
+                                      <p className="px-3 py-2 text-slate-800 text-xs">電話で相談する</p>
                                       <PhoneIcon 
-                                      className="border-gray-100 rounded-full p-1 m-1 bg-gray-100 text-slate-600" 
+                                      className="border-slate-800 rounded-full p-1 m-1 bg-slate-800 text-white" 
                                      
                                       w={26} h={26}
                                       />
@@ -121,14 +134,14 @@ export default function DrawerContents () {
                               w-full h-full inline-block 
                               flex justify-center items-center">
                                 <div className="w-full flex justify-start items-center gap-5">
-                                    <ChevronRightIcon w={26} h={26} />
-                                    <p className='text-white text-center'>事業内容</p>
+                                    <ChevronRightIcon className="text-slate-600" w={26} h={26} />
+                                    <p className='text-slate-600 text-center'>事業内容</p>
                                 </div>
                               </Link>
                               </li>
 
                               <div className="w-full flex items-center">
-                                <div className="flex-1 border-b border-gray-100/30"></div>  
+                                <div className="flex-1 border-b border-slate-500-20"></div>  
                               </div>
 
                               <li className="w-full h-14">
@@ -138,14 +151,14 @@ export default function DrawerContents () {
                               flex justify-center items-center
                               ">
                                 <div className="w-full flex justify-start items-center gap-5">
-                                    <ChevronRightIcon w={26} h={26} />
-                                    <p className='text-white text-center'>設備案内</p>
+                                    <ChevronRightIcon className="text-slate-600" w={26} h={26} />
+                                    <p className='text-slate-600 text-center'>設備案内</p>
                                 </div>
                               </Link>
                               </li>
 
                               <div className="w-full flex items-center">
-                                <div className="flex-1 border-b border-gray-100/30"></div>  
+                                <div className="flex-1 border-b border-slate-500-20"></div>  
                               </div>
 
                               <li className="w-full h-14">
@@ -154,14 +167,14 @@ export default function DrawerContents () {
                               w-full h-full inline-block
                               flex justify-center items-center">
                                 <div className="w-full flex justify-start items-center gap-5">
-                                    <ChevronRightIcon w={26} h={26} />
-                                    <p className='text-white text-center'>会社概要</p>
+                                    <ChevronRightIcon className="text-slate-600" w={26} h={26} />
+                                    <p className='text-slate-600 text-center'>会社概要</p>
                                 </div>
                               </Link>
                               </li>
 
                               <div className="w-full flex items-center">
-                                <div className="flex-1 border-b border-gray-100/30"></div>  
+                                <div className="flex-1 border-b border-slate-500-20"></div>  
                               </div>
 
                               <li className="w-full h-14">
@@ -171,8 +184,8 @@ export default function DrawerContents () {
                               flex justify-center items-center
                               ">
                                 <div className="w-full flex justify-start items-center gap-5">
-                                    <ChevronRightIcon w={26} h={26} />
-                                    <p className='text-white text-center'>お問い合わせ</p>
+                                    <ChevronRightIcon className="text-slate-600" w={26} h={26} />
+                                    <p className='text-slate-600 text-center'>お問い合わせ</p>
                                 </div>
                               </Link>
                               </li>
@@ -183,7 +196,7 @@ export default function DrawerContents () {
                           flex flex-col justify-center items-center gap-2
                           '>
                               <Link href="#" onClick={onClose}>
-                                          <p className='text-xxs text-slate-100'>プライバシーポリシー</p>
+                                          <p className='text-xxs text-slate-600/50'>プライバシーポリシー</p>
                               </Link>
                               <small className='text-xxs text-slate-400'><span className="text-slate-400">©</span>&nbsp;2023&nbsp;株式会社エムパッケージ</small>
                           </div>
