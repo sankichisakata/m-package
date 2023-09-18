49ecfcc9c0431c9b44f23c0f5e4938abed996b0f
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2023-09-18 23:36:20 +0900

    drawer背景編集

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,8 @@ const config: Config = {
         'safari-screen' : '100dvh'
       },
       backgroundImage: {
+       'tell01' : "url('/img/tell01.jpg')" ,
+
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
