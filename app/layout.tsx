8b00c9bceda2d5deee5572955c117c41131b3b36
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2023-09-20 22:52:48 +0900

    main => overscroll-contain追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className={inter.className}>
         <Header/>
-        <main>
+        <main className='overscroll-contain'>
           <Providers>
           {children}
           </Providers>
