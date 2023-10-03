5d5c666a13069563728e91b310f8ea86e256410c
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2023-10-04 01:28:43 +0900

    company編集

--- components/elements/button/contactButton.tsx
+++ components/elements/button/contactButton.tsx
@@ -12,8 +12,8 @@ const ContactButton = () => {
             <Link href="/contact" className="bg-green-600 border border-green-600 rounded-full">
               
                 <div className="flex justify-center items-center">
-                  <p className="px-3 md:px-4 lg:px-6 py-1 md:py-2 lg:py-3 text-white text-xs md;text-sm lg:text-base font-semibold">お問い合わせ</p>
-                  <HiMail className="border-slate-800 rounded-full p-1 m-1 bg-white text-xxl lg:text-xxxl"/>
+                  <p className="px-3 md:px-4 lg:px-6 py-1 md:py-2 lg:py-3 text-white text-xs md:text-sm lg:text-base font-semibold">お問い合わせ</p>
+                  <HiMail className="border-white rounded-full p-1 m-1 bg-white text-xxl lg:text-[2rem]"/>
                   </div>
               
             </Link>
--- components/elements/footerContact/footerContact.tsx
+++ components/elements/footerContact/footerContact.tsx
@@ -48,34 +48,33 @@ export default function FooterContact () {
                 >
                     
                     <div className='flex flex-col justify-center items-center font-semibold'>
-                        <p className="text-base lg:text-lg text-center">お問い合わせ</p>
-                        <p className="text-xs lg:text-sm">CONTACT</p>
+                        <p className="text-xl lg:text-xxxl text-center">お問い合わせ</p>
+                        <p className="text-sm lg:text-lg">CONTACT</p>
                     </div>
                     
 
                     
-                    <div className="text-xxs lg:text-xs text-center font-medium">
-                        <p>お客様のご要望に応じた様々なご提案が可能でございます。</p>
+                    <div className="text-sm lg:text-base text-center font-semibold">
                         <p>お気軽にご相談ください。</p>
                     </div>
                    
 
                     
                     {/* 電話お問い合わせ */}
-                    <div className="text-center text-xxs lg:text-xs">
-                        <p>電話でのお問い合わせ</p>
-                        <p className="text-xs lg:text-sm font-semibold">048-287-8055</p>
+                    <div className="text-center text-xs lg:text-sm">
+                        <p>お急ぎの方は、電話でお問い合わせ</p>
+                        <p className="text-base lg:text-lg font-semibold">048-287-8055</p>
                         <p>平日 9:00 ~ 17:30</p>
                     </div>
 
                     {/* メール問い合わせ */}
                     <div className="text-center">
-                        <p className='text-xs lg:text-base font-semibold'>お問い合わせはこちらから</p>
+                        <p className='text-sm lg:text-base font-semibold'>お問い合わせはこちらから</p>
 
-                        <div className='w-40 lg:w-60 mx-auto py-2 lg:py-4'>
-                                <div className="flex justify-center items-center border border-slate-500/20 rounded-full">
-                                    <p className="px-3 lg:px-10 py-2 lg:py-4 text-sm lg:text-base font-semibold">お問い合わせ</p>
-                                    <HiMail className="border-slate-800 rounded-full p-1 m-1 lg:m-2 bg-slate-800 text-white text-xxxl lg:text-[2rem]"/>
+                        <div className='w-full lg:w-[20rem] mx-auto py-2 lg:py-4'>
+                                <div className="flex justify-center items-center bg-green-600 border border-green-600 rounded-full">
+                                    <p className="px-3 lg:px-10 py-3 lg:py-4 text-white text-lg lg:text-xl font-semibold">お問い合わせ</p>
+                                    <HiMail className="border-slate-800 rounded-full p-1 m-1 lg:m-2 bg-white text-xxxl lg:text-[2rem]"/>
                                 </div>
                         </div>
                     </div>
