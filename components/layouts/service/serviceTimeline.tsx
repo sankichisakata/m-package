43ff670bfb4ac4005b0c691751057734fc2456f1
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2023-10-26 13:36:04 +0900

    timeline編集

--- components/layouts/service/serviceTimeline.tsx
+++ components/layouts/service/serviceTimeline.tsx
@@ -3,8 +3,6 @@ export default function ServiceTimeline () {
     return(
         <>
             <div className="
-            bg-gray-200
-            
             w-full h-full
             flex flex-col justify-center items-center
             ">
@@ -21,62 +19,72 @@ export default function ServiceTimeline () {
                 </div>
 
                 <div className="
-                bg-gray-100
-
+                bg-gray-100 relative
                 w-11/12 lg:w-10/12 max-w-5xl h-full
                 md:p-10 lg:p-0 m-4 md:m-6 lg:m-10 
-                relative">
-                    {/* centerBorder */}
+                flex flex-col justify-center items-center gap-5
+                rounded-lg
+                ">
+                    {/* ボーダー */}
                     <div className="
                     absolute h-full
                     left-1/4 md:left-1/2
                     border-2-2 border-opacity-20 border-gray-700 border"></div>
 
-                    {/* <!-- right timeline --> */}
-                    <div className="
-                    w-full h-full
-                    mb-8 
-                    flex justify-between items-center
-                    ">
-                        <div className="flex items-center"> 
-                            <div className="flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                                <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
-                            </div>
-                        </div>
-                        <div className="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                            <h3 className="mb-3 font-bold text-gray-800 text-xl">ご相談</h3>
-                            <p className="text-sm text-gray-900 text-opacity-100">長年段ボールに携わった経験と知識で最適なご提案を致します。</p>
-                        </div>
-                    </div>
-
-                    {/* <!-- left timeline --> */}
-                    <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                        <div className="order-1 w-5/12"></div>
-                        <div className="flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                            <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
-                        </div>
-                        <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                            <h3 className="mb-3 font-bold text-white text-xl">お見積もり</h3>
-                            <p className="text-sm font-medium text-white text-opacity-100">午前中にいただいたお問い合わせは即日ご連絡致します。</p>
-                        </div>
-                    </div>
-                    
-
-
-
-
-
-
-
-
-                    {/* <!-- 3 right timeline --> */}
+                    {/* <!-- rightTimeline ① --> */}
                     <div className="w-full mb-8 flex justify-center items-center right-timeline">
 
                             <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
 
-                            <div className="w-1/2 md:w-2/12 bg-red-400 order-1 flex justify-center items-center">
+                            <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
                                 <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
                                     <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
+                                </div>
+                            </div>
+
+                            <div className="
+                            w-1/2 md:w-5/12 order-1 bg-gray-400 
+                            rounded-lg shadow-xl">
+                                <div className="w-full p-5">
+                                    <h3 className="mb-3 font-bold text-gray-800 text-xl">ご相談</h3>
+                                    <p className="text-sm text-gray-900 text-opacity-100">長年段ボールに携わった経験と知識で最適なご提案を致します。</p>
+                                </div>
+                            </div>
+                    </div>
+
+
+
+
+                    {/* <!-- leftTimeline ② --> */}
+                    <div className="w-full mb-8 flex justify-center md:flex-row-reverse items-center right-timeline">
+
+                        <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
+
+                        <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
+                            <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                                <h1 className="mx-auto font-semibold text-lg text-white">2</h1>
+                            </div>
+                        </div>
+
+                        <div className="
+                        w-1/2 md:w-5/12 order-1 bg-gray-400 
+                        rounded-lg shadow-xl">
+                            <div className="w-full p-5">
+                                <h3 className="mb-3 font-bold text-gray-800 text-xl">お見積もり</h3>
+                                <p className="text-sm text-gray-900 text-opacity-100">午前中にいただいたお問い合わせは即日ご連絡致します。</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* <!-- rightTimeline ③ --> */}
+                    <div className="w-full mb-8 flex justify-center items-center right-timeline">
+
+                            <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
+
+                            <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
+                                <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                                    <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
                                 </div>
                             </div>
 
@@ -92,26 +100,120 @@ export default function ServiceTimeline () {
                     </div>
 
 
+                    {/* <!-- leftTimeline ④ --> */}
+                    <div className="w-full mb-8 flex justify-center md:flex-row-reverse items-center right-timeline">
 
+                        <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
 
-
-
-
-
-
-
-
-                    {/* <!-- left timeline --> */}
-                    <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-                        <div className="order-1 w-5/12"></div>
-                        <div className="flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-                            <h1 className="mx-auto text-white font-semibold text-lg">4</h1>
+                        <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
+                            <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                                <h1 className="mx-auto font-semibold text-lg text-white">4</h1>
+                            </div>
                         </div>
-                        <div className="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                            <h3 className="mb-3 font-bold text-white text-xl">企画提案</h3>
-                            <p className="text-sm font-medium text-white text-opacity-100">微調整をし、製造へ移っていきます。</p>
+
+                        <div className="
+                        w-1/2 md:w-5/12 order-1 bg-gray-400 
+                        rounded-lg shadow-xl">
+                            <div className="w-full p-5">
+                                <h3 className="mb-3 font-bold text-gray-800 text-xl">企画提案</h3>
+                                <p className="text-sm text-gray-900 text-opacity-100">微調整をし、製造へ移っていきます。</p>
+                            </div>
                         </div>
+
                     </div>
+
+                    {/* <!-- rightTimeline ① --> */}
+                    <div className="w-full mb-8 flex justify-center items-center right-timeline">
+
+                            <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
+
+                            <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
+                                <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                                    <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
+                                </div>
+                            </div>
+
+                            <div className="
+                            w-1/2 md:w-5/12 order-1 bg-gray-400 
+                            rounded-lg shadow-xl">
+                                <div className="w-full p-5">
+                                    <h3 className="mb-3 font-bold text-gray-800 text-xl">ご相談</h3>
+                                    <p className="text-sm text-gray-900 text-opacity-100">長年段ボールに携わった経験と知識で最適なご提案を致します。</p>
+                                </div>
+                            </div>
+                    </div>
+
+
+
+
+                    {/* <!-- leftTimeline ② --> */}
+                    <div className="w-full mb-8 flex justify-center md:flex-row-reverse items-center right-timeline">
+
+                        <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
+
+                        <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
+                            <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                                <h1 className="mx-auto font-semibold text-lg text-white">2</h1>
+                            </div>
+                        </div>
+
+                        <div className="
+                        w-1/2 md:w-5/12 order-1 bg-gray-400 
+                        rounded-lg shadow-xl">
+                            <div className="w-full p-5">
+                                <h3 className="mb-3 font-bold text-gray-800 text-xl">お見積もり</h3>
+                                <p className="text-sm text-gray-900 text-opacity-100">午前中にいただいたお問い合わせは即日ご連絡致します。</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    {/* <!-- rightTimeline ③ --> */}
+                    <div className="w-full mb-8 flex justify-center items-center right-timeline">
+
+                            <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
+
+                            <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
+                                <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                                    <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
+                                </div>
+                            </div>
+
+                            <div className="
+                            w-1/2 md:w-5/12 order-1 bg-gray-400 
+                            rounded-lg shadow-xl">
+                                <div className="w-full p-5">
+                                    <h3 className="mb-3 font-bold text-gray-800 text-xl">お見本作成</h3>
+                                    <p className="text-sm text-gray-900 text-opacity-100">お客様のニーズをカタチにします。</p>
+                                </div>
+                            </div>
+
+                    </div>
+
+
+                    {/* <!-- leftTimeline ④ --> */}
+                    <div className="w-full mb-8 flex justify-center md:flex-row-reverse items-center right-timeline">
+
+                        <div className="hidden md:block w-5/12 order-1 bg-red-400 md:flex-2"></div>
+
+                        <div className="z-[1] w-1/2 md:w-2/12 order-1 flex justify-center items-center">
+                            <div className="flex justify-center items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
+                                <h1 className="mx-auto font-semibold text-lg text-white">4</h1>
+                            </div>
+                        </div>
+
+                        <div className="
+                        w-1/2 md:w-5/12 order-1 bg-gray-400 
+                        rounded-lg shadow-xl">
+                            <div className="w-full p-5">
+                                <h3 className="mb-3 font-bold text-gray-800 text-xl">企画提案</h3>
+                                <p className="text-sm text-gray-900 text-opacity-100">微調整をし、製造へ移っていきます。</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
                 </div>
             </div>
 
