78117e7f32542ea44cba3083c874ded6df462b0f
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2024-01-07 12:36:54 +0900

    news編集

--- components/elements/libs/microcms.tsx
+++ components/elements/libs/microcms.tsx
@@ -18,18 +18,18 @@ export type News = {
  eyecatch?: MicroCMSImage;
 } & MicroCMSDate;
 
-if (!process.env.MICROCMS_SERVICE_DOMAIN) {
-throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+if (!process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN) {
+throw new Error("NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN is required");
 }
 
-if (!process.env.MICROCMS_API_KEY) {
+if (!process.env.NEXT_PUBLIC_MICROCMS_API_KEY) {
 throw new Error("MICROCMS_API_KEY is required");
 }
 
 // API取得用のクライアントを作成
 export const client = createClient({
-    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-    apiKey: process.env.MICROCMS_API_KEY,
+    serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN,
+    apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY,
 });
 
 // top:ニュース一覧を取得
