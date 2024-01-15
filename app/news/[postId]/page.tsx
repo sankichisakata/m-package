5a6fc647272e435d19adb984a95d017e90b0eb4a
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2024-01-15 21:28:02 +0900

    news.id編集

--- app/news/[postId]/page.tsx
+++ app/news/[postId]/page.tsx
@@ -6,33 +6,33 @@ import { FadeInBack } from '@/components/elements/observer/fadeInBack'
 import { HiChevronDoubleRight } from 'react-icons/hi'
 import Link from 'next/link';
 import Date from '@/components/elements/date-fns/date';
-import { Metadata, ResolvingMetadata } from 'next'
+// import { Metadata, ResolvingMetadata } from 'next'
 
-type Props = {
-    params: { id: string }
-    searchParams: { [key: string]: string | string[] | undefined }
-  }
+// type Props = {
+//     params: { id: string }
+//     searchParams: { [key: string]: string | string[] | undefined }
+//   }
 
-export async function generateMetadata(
-    { params, searchParams }: Props,
-    parent: ResolvingMetadata
-  ): Promise<Metadata> {
-    // read route params
-    const id = params.id
+// export async function generateMetadata(
+//     { params, searchParams }: Props,
+//     parent: ResolvingMetadata
+//   ): Promise<Metadata> {
+//     // read route params
+//     const id = params.id
    
-    // fetch data
-    const product = await fetch(`https://m-package.com/news/${id}`).then((res) => res.json())
+//     // fetch data
+//     const product = await fetch(`https://m-package.com/news/${id}`).then((res) => res.json())
    
-    // optionally access and extend (rather than replace) parent metadata
-    // const previousImages = (await parent).openGraph?.images || []
+//     // optionally access and extend (rather than replace) parent metadata
+//     // const previousImages = (await parent).openGraph?.images || []
    
-    return {
-      title: product.title,
-    //   openGraph: {
-    //     images: ['/some-specific-page-image.jpg', ...previousImages],
-    //   },
-    }
-  }
+//     return {
+//       title: product.title,
+//     //   openGraph: {
+//     //     images: ['/some-specific-page-image.jpg', ...previousImages],
+//     //   },
+//     }
+//   }
 
 export async function generateStaticParams() {
     const { contents } = await getList();
