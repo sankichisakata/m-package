db3d0567c4c57633f41ab5a3872969c8f019f351
sankichisakata <142443250+sankichisakata@users.noreply.github.com>
2024-02-15 19:48:54 +0900

    .htacsess変更

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = { 
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: "/:path*",
-    //             has: [{ type: "host", value: "m-package.com" }],
-    //             destination: "https://www.m-package.com/:path*",
-    //             permanent: true,
-    //         },
-    //     ];
-    // },
-    output: 'export',
+    // output: 'export',
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
